81c42a08d289918370225e0e1c2b4639aa42e713
David Windell <david@outeredgeuk.com>
2017-06-08 16:40:20 +0100

    Make sed actually work

--- run.sh
+++ run.sh
@@ -4,8 +4,8 @@ sed -i 's/root \/var\/www/root \/var\/www\/site/g' /etc/nginx/conf.d/default.con
 
 if [[ $APPLICATION_ENV = "dev" ]]
 then
-    sed 's/autostart=true/autostart=false/g' /etc/supervisor/conf.d/supervisord.conf
-    sed 's/autostart=false/autostart=true/g' /etc/supervisor/conf.d/mkdocs.conf
+    sed -i 's/autostart=true/autostart=false/g' /etc/supervisor/conf.d/supervisord.conf
+    sed -i 's/autostart=false/autostart=true/g' /etc/supervisor/conf.d/mkdocs.conf
 fi
 
 /usr/bin/supervisord
